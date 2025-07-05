96f609ee813b373a4409b1afd0496227e4786cb3
GodReaper <brajutso@gmail.com>
2025-07-05 15:21:15 +0530

    removed for loop

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,3 +1,4 @@
+from logging import Logger
 from flask import Blueprint, jsonify
 from app.extensions import get_events_collection
 
@@ -6,6 +7,7 @@ api_bp = Blueprint('api', __name__, url_prefix='/api')
 @api_bp.route('/events', methods=['GET'])
 def get_events():
     events = list(get_events_collection().find().sort('timestamp', -1).limit(20))
-    for e in events:
-        e['_id'] = str(e['_id'])
+
+    print(type(events))
     return jsonify({'events': events})
+
